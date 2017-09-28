274349f473e419aea4670e8f4c6a321de4a213e6
Guilherme Calderaro <guilhermecald96@gmail.com>
2017-09-28 16:19:47 -0300

    Adicionei interface de usuário e passei o reconhecimendo para classe ConjuntoProd

--- src/formais/Formais.java
+++ src/formais/Formais.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import rules.Producao;
+import ui.Tela;
 
 /**
  *
@@ -20,29 +21,12 @@ public class Formais {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<String> nTerm = new LinkedList<>(Arrays.asList("S","A","B"));
-        List<String> term = new LinkedList<>(Arrays.asList("a","b","c"));
         
-        String text = "S:aS,bS,c\n"
-                    + "A:a,b,&";
+        Tela tela = new Tela();
+        tela.setVisible(true);
+
         
-        List<Producao> conjProd = new LinkedList<>();
-        
-        String[] auxText = text.split("\n");
-        
-        for (int i = 0; i < auxText.length; i++) {
-            String[] auxProd = auxText[i].split(":");
-            
-            conjProd.add(new Producao(auxProd[0]));
-            
-            String[] auxSent = auxProd[1].split(",");
-            System.out.println(auxProd[1]);
-            for (int j = 0; j < auxSent.length ; j++) {
-                conjProd.get(i).addProd(auxSent[j]);
-                System.out.println(auxSent[j]);
-            }
-        }
-        
+
         
         
         
--- src/rules/ConjuntoProd.java
+++ src/rules/ConjuntoProd.java
@@ -5,10 +5,66 @@
  */
 package rules;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author m93492
  */
 public class ConjuntoProd {
+    List<Producao> conjProd;
+    List<String> conjNterm;
+    List<String> conjTerm;
+    String termIni;
+
+    public ConjuntoProd() {
+        this.conjProd = new LinkedList<>();
+        this.conjNterm = new LinkedList<>();
+        this.conjTerm = new LinkedList<>();
+    }
+    
+    
+    public void setProd(String[] auxText){
+        
+        for (int i = 0; i < auxText.length; i++) {
+            String[] auxProd = auxText[i].split(":");
+            
+            conjProd.add(new Producao(auxProd[0]));
+            
+            String[] auxSent = auxProd[1].split(",");
+            System.out.println(auxProd[1]);
+            for (int j = 0; j < auxSent.length ; j++) {
+                conjProd.get(i).addProd(auxSent[j]);
+                //System.out.println(auxSent[j]);
+            }
+        }
+    }
+    
+    public void setConjuntos(String[] auxText){
+        for(String s: auxText){
+            
+        }
+    }
+
+    @Override
+    public String toString() {
+        String text = "";
+        
+        for (Producao p :conjProd) {
+            text += "" + p.nTerm + ":" + p.prod.get(0);
+            
+            if(p.prod.size()>1){
+                for (int i = 1; i < p.prod.size(); i++) {
+                    text += "," + p.prod.get(i);
+                }
+            }
+            text += "\n";
+        }
+        
+        return text;
+    }
+  
+
     
 }
